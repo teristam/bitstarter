52cae8e0f21846871a48a8a42cd13d5a5bbf0745
Teris TAM <teristam@startup.com>
2013-07-11 15:13:56 +0000

    fixed some bug

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.get('/', function(request, response) {
   
   var fs=require('fs');
     var buffer=new Buffer(fs.readFileSync('index.html');  
-    response.send(buffer.toString());
+    response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
