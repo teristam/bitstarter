162c9e91efbc70d43c2c773a9dcbef2202b14941
Teris TAM <teristam@startup.com>
2013-07-11 15:09:46 +0000

    Modified again

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  
+  var fs=require('fs');
+    var buffer=new Buffer(fs.readFileSync('index.html');  
+    response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
